dc1908f3f0025380b83da61eca2882c6b59d6c5c
Sunil-Pradhan <pradhan.sunil@outlook.com>
2020-07-10 14:26:58 +0530

    Updates

---
+++ precache-manifest.e053e597e79a010b8c5a64f523905907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b173ecdea58565877499c24bdf7e838",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "e00fa0d4a3206ab14484",
+    "url": "/react-todo-app/static/css/main.403149c2.chunk.css"
+  },
+  {
+    "revision": "a6adec9585eed90b4354",
+    "url": "/react-todo-app/static/js/2.14a044fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.14a044fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00fa0d4a3206ab14484",
+    "url": "/react-todo-app/static/js/main.d8dfd83e.chunk.js"
+  },
+  {
+    "revision": "5f1a8b4c1d93b1bf4f99",
+    "url": "/react-todo-app/static/js/runtime-main.aecdcad4.js"
+  }
+]);
